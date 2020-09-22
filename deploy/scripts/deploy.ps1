c4f114eb7324e3ac5d820a8af9f2ef4240d924f1
Dan Silver <dansil@microsoft.com>
2020-09-21 17:53:45 -0700

    add back new-password

--- deploy/scripts/deploy.ps1
+++ deploy/scripts/deploy.ps1
@@ -517,6 +517,29 @@ Function Select-ResourceGroup() {
 }
 
 #******************************************************************************
+# Generate a random password
+#******************************************************************************
+Function New-Password() {
+    param(
+        $length = 15
+    )
+    $punc = 46..46
+    $digits = 48..57
+    $lcLetters = 65..90
+    $ucLetters = 97..122
+    $password = `
+        [char](Get-Random -Count 1 -InputObject ($lcLetters)) + `
+        [char](Get-Random -Count 1 -InputObject ($ucLetters)) + `
+        [char](Get-Random -Count 1 -InputObject ($digits)) + `
+        [char](Get-Random -Count 1 -InputObject ($punc))
+    $password += get-random -Count ($length - 4) `
+        -InputObject ($punc + $digits + $lcLetters + $ucLetters) |`
+        ForEach-Object -begin { $aa = $null } -process { $aa += [char]$_ } -end { $aa }
+
+    return $password
+}
+
+#******************************************************************************
 # Get env file content from deployment
 #******************************************************************************
 Function Get-EnvironmentVariables() {
@@ -1110,7 +1133,6 @@ Function Test-All-Deployment-Options() {
 $ErrorActionPreference = "Stop"
 $script:ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 $script:interactive = !$script:context
-. ".\util\password-generator.ps1"
 
 $script:requiredProviders = @(
     "microsoft.devices",

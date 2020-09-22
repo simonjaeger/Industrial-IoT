585b354d69a499e753c9361c9ab2463455e49815
Dan Silver <dansil@microsoft.com>
2020-09-21 17:59:10 -0700

    revert password

--- tools/e2etesting/DeployEdgeVM.ps1
+++ tools/e2etesting/DeployEdgeVM.ps1
@@ -28,13 +28,12 @@ if ($prereqsDeployment.ProvisioningState -ne "Succeeded") {
 Write-Host "Created MSI $($msi.Parameters.managedIdentityName.Value) with resource id $($prereqsDeployment.Outputs.managedIdentityResourceId.Value)"
 
 # Deploy edge and simulation vms
-. "../../deploy/scripts/util/password-generator.ps1"
 $templateParameters = @{
     # secrets pcs-dps-idscope and pcs-dps-connstring are retrieved from this keyvault
     "keyVaultName" = $keyVaultName
 	"managedIdentityResourceId" = $prereqsDeployment.Outputs.managedIdentityResourceId.Value
     "numberOfLinuxGateways" = 1
-    "edgePassword" = New-Password
+    "edgePassword" = [System.Web.Security.Membership]::GeneratePassword(15, 5)
 }
 
 $simulationTemplate = [System.IO.Path]::Combine($templateDir, "azuredeploy.simulation.json")
